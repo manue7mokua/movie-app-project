1346c8b62f8fe6da8592ba37ce91b878f279bc86
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-06-13 00:01:45 -0700

    Created header with list of various app pages and user's profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import './Header.css'
 import SearchForm from '../SearchForm/SearchForm'
+import logo_icon from '../../assets/logo.png'
+import bell_icon from '../../assets/bell.png'
+import profile_icon from '../../assets/profile.png'
+import menu_icon from '../../assets/menu.png'
 
 const Header = () => {
   return (
-    <div className='header-content'>
-        <h1 className='golden-rod-text'>ZondIn</h1>
-
+    <div className='header'>
+        <div className='header-left'>
+            <img src={logo_icon} alt='' className='app-logo'/>
+            <ul>
+                <li className='golden-rod-text medium-text'>Home</li>
+                <li className='golden-rod-text medium-text'>Movies</li>
+                <li className='golden-rod-text medium-text'>Movies</li>
+                <li className='golden-rod-text medium-text'>TV Shows</li>
+                <li className='golden-rod-text medium-text'>Popular</li>
+                <li className='golden-rod-text medium-text'>My List</li>
+                <li className='golden-rod-text medium-text'>Filter</li>
+            </ul>
+        </div>
+        <div className='header-right'>
+            <img src={bell_icon} alt='' className='icons bell-icon'/>
+            <div className='header-profile'>
+            <img src={profile_icon} alt='' className='icons profile-icon'/>
+            <img src={menu_icon} alt='' className='icons menu-icon'/>
+            <div className='dropdown'>
+              <p className='golden-rod-text medium-text'>Signing Out?</p>
+            </div>
+            </div>
+        </div>
     </div>
   )
 }
